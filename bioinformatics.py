ae53d4554b71548941f1351deb1fed5c0b17e153
Joe Uchytil <167820045+Juchytil@users.noreply.github.com>
2025-01-28 17:42:00 -0800

    Update bioinformatics.py

--- bioinformatics.py
+++ bioinformatics.py
@@ -1,6 +1,8 @@
 # My Rosalind Solutions
 
-## Counting Nucleotides
+sequence = 'ATGCGGGCGAGCGTTTCGGAGGGTATTTATTATCTTTCTATCATTTTTTAGGGGAGGATTTTAGGGGATTATCTCTCGATCGATTATCGATCC'
+
+# Counting Nucleotides (DNA)
 
 def nuc_count(seq):
     A_count = seq.count('A')
@@ -10,13 +12,20 @@ def nuc_count(seq):
     counts = (A_count,T_count,G_count,C_count)
     return counts
 
-## DNA to RNA
+## DNA to RNA (RNA)
 
 def dna_to_rna(seq):
   rna = seq.replace('T','U')
   return rna
 
-## GC Content Function
+## Reverse Compliment Strand (REVC)
+
+def reverse_compliment_seq(seq):
+  compliment_lib = str.maketrans({'A':'T','T':'A','C':'G','G':'C'})
+  rev_comp_seq = seq.translate(compliment_lib)[::-1]
+  return rev_comp_seq
+
+## GC Content Function (GC)
 
 def GC_content(sequence):
     seq_len = sequence.count('')
@@ -26,10 +35,44 @@ def GC_content(sequence):
     GC_percent = (GC_value/seq_len)*100
     return GC_percent
 
-## Reverse Compliment Strand
+## Point mutation count (HAMM)
 
-def reverse_compliment_seq(seq):
-  compliment_lib = str.maketrans({'A':'T','T':'A','C':'G','G':'C'})
-  rev_comp_seq = seq.translate(compliment_lib)[::-1]
-  return rev_comp_seq
+def mutations_count(seq_a, seq_b):
+  mutation = 0
+  for i in range(len(seq_a)):
+    if seq_a[i] != seq_b[i]:
+      mutation += 1
+  return mutation
+
+## RNA to protein (PROT)
+
+RNA_codon_table = {
+'A': ('GCU', 'GCC', 'GCA', 'GCG'),
+'C': ('UGU', 'UGC'),
+'D': ('GAU', 'GAC'),
+'E': ('GAA', 'GAG'),
+'F': ('UUU', 'UUC'),
+'G': ('GGU', 'GGC', 'GGA', 'GGG'),
+'H': ('CAU', 'CAC'),
+'I': ('AUU', 'AUC', 'AUA'),
+'K': ('AAA', 'AAG'),
+'L': ('UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'),
+'M': ('AUG',),
+'N': ('AAU', 'AAC'),
+'P': ('CCU', 'CCC', 'CCA', 'CCG'),
+'Q': ('CAA', 'CAG'),
+'R': ('CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'),
+'S': ('UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'),
+'T': ('ACU', 'ACC', 'ACA', 'ACG'),
+'V': ('GUU', 'GUC', 'GUA', 'GUG'),
+'W': ('UGG',),
+'Y': ('UAU', 'UAC'),}
+
+rna_sequence = sequence.replace('T','U')
+
+def rna_to_prot(rna_seq):
+  for i in range(0,len(rna_seq),3):
+    print(rna_seq[i:i+3])
+
+print(rna_to_prot(rna_sequence))
 
