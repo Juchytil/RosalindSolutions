f060554c258dc592f542d365b30bcacb4360ea1d
Joe Uchytil <167820045+Juchytil@users.noreply.github.com>
2025-01-28 18:12:30 -0800

    Update bioinformatics.py

--- bioinformatics.py
+++ bioinformatics.py
@@ -46,33 +46,30 @@ def mutations_count(seq_a, seq_b):
 
 ## RNA to protein (PROT)
 
-RNA_codon_table = {
-'A': ('GCU', 'GCC', 'GCA', 'GCG'),
-'C': ('UGU', 'UGC'),
-'D': ('GAU', 'GAC'),
-'E': ('GAA', 'GAG'),
-'F': ('UUU', 'UUC'),
-'G': ('GGU', 'GGC', 'GGA', 'GGG'),
-'H': ('CAU', 'CAC'),
-'I': ('AUU', 'AUC', 'AUA'),
-'K': ('AAA', 'AAG'),
-'L': ('UUA', 'UUG', 'CUU', 'CUC', 'CUA', 'CUG'),
-'M': ('AUG',),
-'N': ('AAU', 'AAC'),
-'P': ('CCU', 'CCC', 'CCA', 'CCG'),
-'Q': ('CAA', 'CAG'),
-'R': ('CGU', 'CGC', 'CGA', 'CGG', 'AGA', 'AGG'),
-'S': ('UCU', 'UCC', 'UCA', 'UCG', 'AGU', 'AGC'),
-'T': ('ACU', 'ACC', 'ACA', 'ACG'),
-'V': ('GUU', 'GUC', 'GUA', 'GUG'),
-'W': ('UGG',),
-'Y': ('UAU', 'UAC'),}
+rna_codon_table = {"UUU" : "F", "CUU" : "L", "AUU" : "I", "GUU" : "V",
+           "UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V",
+           "UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V",
+           "UUG" : "L", "CUG" : "L", "AUG" : "M", "GUG" : "V",
+           "UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A",
+           "UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D",
+           "UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+           "UAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+           "UAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G",
+           "UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+           "UGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G"}
 
 rna_sequence = sequence.replace('T','U')
 
 def rna_to_prot(rna_seq):
+  protien = ''
   for i in range(0,len(rna_seq),3):
-    print(rna_seq[i:i+3])
-
+    protien += rna_codon_table[rna_seq[i:i+3]]
+  return protien
+    
 print(rna_to_prot(rna_sequence))
 
